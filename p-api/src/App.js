3f5621895e105d0d75e5aa6eae16b7436e5ba428
RioMulya <riomulya75@gmail.com>
2022-03-18 15:38:26 +0700

    Interaksi Dengan API (GET)

--- p-api/src/App.js
+++ p-api/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataApi: [],
+    };
+  }
+  componentDidMount() {
+    // fetch("https://jsonplaceholder.typicode.com/photos")
+    //   .then((response) => response.json())
+    //   .then((res) =>
+    //     this.setState({
+    //       dataApi: res,
+    //     })
+    //   );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
+      this.setState({
+        dataApi: res.data,
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Helo Api</h1>
+        {this.state.dataApi.map((data, index) => {
+          return (
+            <span key={index} style={{ margin: "5px" }}>
+              <p>No : {data.id}</p>
+              <img src={data.thumbnailUrl} alt={data.title} />
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
