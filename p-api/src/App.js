2b1075e0ba3ff717dd34e96f069606a11df946eb
RioMulya <riomulya75@gmail.com>
2022-03-18 17:02:25 +0700

    Interaksi Dengan API (DELETE)

--- p-api/src/App.js
+++ p-api/src/App.js
@@ -6,45 +6,65 @@ class App extends Component {
     this.state = {
       dataApi: [],
     };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.reloadData = this.reloadData.bind(this);
   }
-  filter(value) {
-    if (value === 1) {
-      return;
-    }
+  reloadData() {
+    axios.get("http://localhost:3004/posts").then((res) => {
+      this.setState({
+        dataApi: res.data,
+      });
+    });
+  }
+  handleRemove(e) {
+    fetch(`http://localhost:3004/posts/${e.target.value}`, {
+      method: "DELETE",
+    }).then(() => this.reloadData());
   }
   componentDidMount() {
-    // fetch("https://jsonplaceholder.typicode.com/photos")
+    // fetch("http://localhost:3004/posts")
     //   .then((response) => response.json())
     //   .then((res) =>
     //     this.setState({
     //       dataApi: res,
     //     })
     //   );
-
-    axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
-      this.setState({
-        dataApi: res.data,
-      });
-    });
+    this.reloadData();
   }
   render() {
     return (
       <div>
-        <h1>Helo Api</h1>
-        {this.state.dataApi.map((data, index) => {
-          if (data.albumId === 1 || data.albumId === 2) {
-            return (
-              <span key={index} style={{ margin: "5px" }}>
-                <p>No : {data.id}</p>
-                <p>Album Ke : {data.albumId}</p>
-                <p>
-                  Judul : <strong>{data.title}</strong>
-                </p>
-                <img src={data.thumbnailUrl} alt={data.title} />
-              </span>
-            );
-          }
-        })}
+        <center>
+          <h1>Helo Api</h1>
+          {this.state.dataApi.map((data, index) => {
+            if (
+              data.albumId === 1 ||
+              data.albumId === 2 ||
+              data.albumId === 12 ||
+              data.albumId === 22 ||
+              data.albumId === 32 ||
+              data.albumId === 42
+            ) {
+              return (
+                <span key={index} style={{ margin: "5px" }}>
+                  <p>No : {data.id}</p>
+                  <p>Album Ke : {data.albumId}</p>
+                  <p>
+                    Judul : <strong>{data.title}</strong>
+                  </p>
+                  <img src={data.thumbnailUrl} alt={data.title} />
+                  <p>
+                    <button value={data.id} onClick={this.handleRemove}>
+                      Delete
+                    </button>
+                  </p>
+                  <br />
+                  <hr />
+                </span>
+              );
+            }
+          })}
+        </center>
       </div>
     );
   }
